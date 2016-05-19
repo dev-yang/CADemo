79ef471674052586aedcdf9e99223d298a72ff1d
yangjunpeng <yangjunpeng@readnovel.com>
2016-05-19 17:18:52 +0800

    Create new project

---
+++ CADemo_BasicDemo/CADemo_BasicDemoUITests/CADemo_BasicDemoUITests.m
@@ -0,0 +1,40 @@
+//
+//  CADemo_BasicDemoUITests.m
+//  CADemo_BasicDemoUITests
+//
+//  Created by 杨俊鹏 on 16/5/19.
+//  Copyright © 2016年 杨俊鹏. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CADemo_BasicDemoUITests : XCTestCase
+
+@end
+
+@implementation CADemo_BasicDemoUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
